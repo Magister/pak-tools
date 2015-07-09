0111fa1fb38d8879aae35a4bc55a23939ff12719
Ethan Randall <iJunkie22@gmail.com>
2015-07-09 00:12:49 -0400

    Simplified the arg length check

--- pack.py
+++ pack.py
@@ -10,20 +10,23 @@ from grit.format.data_pack import DataPack
 BINARY, UTF8, UTF16 = range(3)
 
 def main(*args):
-    if len(args) > 1:
-        directory = args[1]
-        out_file = directory+".pak"
-        files = os.listdir(directory)
-        data = {}
-        print "Reading files..."
-        for fp in files:
-            id_str = int(os.path.splitext(fp)[0])
-            with open(os.path.join(directory, fp), "rb") as fp:
-                contents = fp.read()
-            data[id_str] = contents
-        print "Writing data..."
-        DataPack.WriteDataPack(data, out_file, BINARY)
-        print "Finished."
+    if len(args) < 2:
+        return 1
+
+    directory = args[1]
+    out_file = directory+".pak"
+    files = os.listdir(directory)
+    data = {}
+    print "Reading files..."
+    for fp in files:
+        id_str = int(os.path.splitext(fp)[0])
+        with open(os.path.join(directory, fp), "rb") as fp:
+            contents = fp.read()
+        data[id_str] = contents
+    print "Writing data..."
+    DataPack.WriteDataPack(data, out_file, BINARY)
+    print "Finished."
+    return 0
 
 if __name__ == '__main__':
     main(*sys.argv)
--- unpack.py
+++ unpack.py
@@ -12,41 +12,43 @@ BINARY, UTF8, UTF16 = range(3)
 
 
 def main(*args):
-    if len(args) > 1:
-        fp = args[1]
-        directory = os.path.splitext(fp)[0] + "\\"
-        if os.path.exists(directory):
-            shutil.rmtree(directory)
-        os.makedirs(directory)
-        print "Reading file..."
-        data = DataPack.ReadDataPack(fp)
-        print "File encoding: " + str(data.encoding)
-        print "Saving data..."
-        for (resource_id, text) in data.resources.iteritems():
-            ftype = 'bin'
-            fheader = text.strip()[0:3]
-            if fheader[0:1] == '<':
-                ftype = 'html'
-            elif fheader[0:1] == '\x89':
-                ftype = 'png'
-            elif fheader[0:1] == '/':
-                ftype = 'js'
-            elif fheader[0:1] == '.':
-                ftype = 'css'
-            elif fheader == 'GIF':
-                ftype = 'gif'
-            elif text.find("function(") > 0:
-                ftype = 'js'
-            elif text.find("body {") > 0:
-                ftype = 'css'
-            elif text.find("font-size:") > 0:
-                ftype = 'css'
-            elif fheader == 'RIF':
-                ftype = 'wav'
-            output_file = "%s/%s.%s" % (directory, resource_id, ftype)
-            with open(output_file, "wb") as fd:
-                fd.write(text)
-        print "Finished."
+    if len(args) < 2:
+        return 1
+    fp = args[1]
+    directory = os.path.splitext(fp)[0] + "\\"
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.makedirs(directory)
+    print "Reading file..."
+    data = DataPack.ReadDataPack(fp)
+    print "File encoding: " + str(data.encoding)
+    print "Saving data..."
+    for (resource_id, text) in data.resources.iteritems():
+        ftype = 'bin'
+        fheader = text.strip()[0:3]
+        if fheader[0:1] == '<':
+            ftype = 'html'
+        elif fheader[0:1] == '\x89':
+            ftype = 'png'
+        elif fheader[0:1] == '/':
+            ftype = 'js'
+        elif fheader[0:1] == '.':
+            ftype = 'css'
+        elif fheader == 'GIF':
+            ftype = 'gif'
+        elif text.find("function(") > 0:
+            ftype = 'js'
+        elif text.find("body {") > 0:
+            ftype = 'css'
+        elif text.find("font-size:") > 0:
+            ftype = 'css'
+        elif fheader == 'RIF':
+            ftype = 'wav'
+        output_file = "%s/%s.%s" % (directory, resource_id, ftype)
+        with open(output_file, "wb") as fd:
+            fd.write(text)
+    print "Finished."
+    return 0
 
 
 if __name__ == '__main__':
