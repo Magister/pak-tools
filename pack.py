22def78e6496b3fffa64dfbf81a4c5b1d678d849
Ethan Randall <iJunkie22@gmail.com>
2015-07-08 23:55:01 -0400

    Changed 2-space indentations to 4-space (to meet PEP8)

--- pack.py
+++ pack.py
@@ -10,20 +10,20 @@ from grit.format.data_pack import DataPack
 BINARY, UTF8, UTF16 = range(3)
 
 def main():
-  if len(sys.argv) > 1:
-    directory = sys.argv[1]
-    out_file = directory+".pak"
-    files = os.listdir(directory)
-    data = {}
-    print "Reading files..."
-    for file in files:
-        id = int(os.path.splitext(file)[0])
-        with open(os.path.join(directory, file), "rb") as file:
-          contents = file.read()
-    	data[id] = contents
-    print "Writing data..."
-    DataPack.WriteDataPack(data, out_file, BINARY)
-    print "Finished."
+    if len(sys.argv) > 1:
+        directory = sys.argv[1]
+        out_file = directory+".pak"
+        files = os.listdir(directory)
+        data = {}
+        print "Reading files..."
+        for file in files:
+            id = int(os.path.splitext(file)[0])
+            with open(os.path.join(directory, file), "rb") as file:
+                contents = file.read()
+            data[id] = contents
+        print "Writing data..."
+        DataPack.WriteDataPack(data, out_file, BINARY)
+        print "Finished."
 
 if __name__ == '__main__':
-  main()
+    main()
